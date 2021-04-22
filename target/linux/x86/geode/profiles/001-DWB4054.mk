36bbc47f17abd7d05ab7cb23af238e401eab1530
leesy24 <leesy24@ubuntu-16.04-64bit>
2021-04-22 17:19:24 +0900

    [DWB4054] assign version to v6.4

--- target/linux/x86/geode/profiles/001-DWB4054.mk
+++ target/linux/x86/geode/profiles/001-DWB4054.mk
@@ -7,7 +7,7 @@
 
 define Profile/DWB4054
 	NAME:=DWB4054 Profile
-	VERSION:=v6.3
+	VERSION:=v6.4
 	PACKAGES:= \
 		avahi-daemon-service-http avahi-daemon-service-ssh avahi-nodbus-daemon \
 		-dnsmasq \
